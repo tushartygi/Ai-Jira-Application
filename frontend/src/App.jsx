d0a7d0035db0d3b6e3480c4a67e9ff3f2196a32f
tushartygi <tushartyagi210@gmail.com>
2025-07-29 20:54:53 +0530

    Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import Header from "./components/Header";
+
+function App() {
+  return <Header> </Header>;
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import "./Components.style.css";
+const Header = () => (
+  <header className="text-center py-5 bg-blue-700">
+    <h1 className="text-3xl font-bold text-white ">Jira Application</h1>
+  </header>
+);
+
+export default Header;
